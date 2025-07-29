6bbab6979eb06abaa2b63659bd86bdf964a9d854
Anusha <anusha.22cse@bmu.edu.in>
2025-07-29 19:19:33 +0530

    Time: 6 ms (21.54%) | Memory: 23.6 MB (17.56%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        // Step 1: Remove leading, trailing and multiple spaces
-        stringstream ss(s);
-        string word, result;
-
-        while (ss >> word) {
+        // Remove leading/trailing/multiple spaces
+        int n = s.size();
+        string result, word;
+        for (int i = 0; i < n;) {
+            while (i < n && s[i] == ' ') i++;  // skip spaces
+            if (i >= n) break;
+            word = "";
+            while (i < n && s[i] != ' ') word += s[i++];
             if (!result.empty()) result = word + " " + result;
             else result = word;
         }
-
         return result;
     }
 };
