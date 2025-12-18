71cb52473a09b91e533f5b223f13a1c4081bb351
Anusha <anushasingh0501@gmail.com>
2025-12-18 22:07:00 +0530

    Time: 3 ms (62.71%) | Memory: 10.1 MB (74.76%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> result;
+        vector<string> board(n, string(n, '.'));
+        
+        vector<bool> col(n, false);
+        vector<bool> diag1(2 * n, false);        // row + col
+        vector<bool> diag2(2 * n, false);        // row - col + n - 1
+        
+        backtrack(0, n, board, col, diag1, diag2, result);
+        return result;
+    }
+
+private:
+    void backtrack(int row, int n, vector<string>& board,
+                   vector<bool>& col, vector<bool>& diag1,
+                   vector<bool>& diag2, vector<vector<string>>& result) {
+        
+        if (row == n) {
+            result.push_back(board);
+            return;
+        }
+        
+        for (int c = 0; c < n; c++) {
+            if (col[c] || diag1[row + c] || diag2[row - c + n - 1])
+                continue;
+            
+            // Place queen
+            board[row][c] = 'Q';
+            col[c] = diag1[row + c] = diag2[row - c + n - 1] = true;
+            
+            backtrack(row + 1, n, board, col, diag1, diag2, result);
+            
+            // Remove queen (backtrack)
+            board[row][c] = '.';
+            col[c] = diag1[row + c] = diag2[row - c + n - 1] = false;
+        }
+    }
+};
