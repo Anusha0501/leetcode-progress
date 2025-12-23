a14a802fc3ea9f0826b92015978e652aa8b2d592
Anusha <anushasingh0501@gmail.com>
2025-12-23 15:45:07 +0530

    Time: 3 ms (29.55%) | Memory: 9.7 MB (94.70%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> current;
-        backtrack(nums, 0, current, result);
-        return result;
+    vector<vector<int>> result;
+
+    void backtrack(int index, vector<int>& nums, vector<int>& current) {
+        // store current subset
+        result.push_back(current);
+
+        for (int i = index; i < nums.size(); i++) {
+            current.push_back(nums[i]);          // choose
+            backtrack(i + 1, nums, current);     // explore
+            current.pop_back();                  // unchoose
+        }
     }
 
-private:
-    void backtrack(vector<int>& nums, int index, vector<int>& current, vector<vector<int>>& result) {
-        // Add the current subset to result
-        result.push_back(current);
-        
-        for (int i = index; i < nums.size(); i++) {
-            current.push_back(nums[i]);           // choose nums[i]
-            backtrack(nums, i + 1, current, result); // move to next index
-            current.pop_back();                   // backtrack
-        }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> current;
+        backtrack(0, nums, current);
+        return result;
     }
 };
