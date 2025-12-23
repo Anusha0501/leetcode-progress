cd9989dcb9db4a21a50d1785d97bf3244e9ea10a
Anusha <anushasingh0501@gmail.com>
2025-12-23 15:50:22 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (57.97%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,9 +3,8 @@ public:
     int singleNumber(vector<int>& nums) {
         int result = 0;
         for (int num : nums) {
-            result ^= num;  // XOR all numbers
+            result ^= num;
         }
         return result;
-        
     }
 };
